365452569c6fb5b49f819f7f4433a78bd339ba6b
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-11 13:24:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ require "pry"
 def oxford_comma(array)
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
+  elsif array.length>2
     array[-1].insert(0, "and ")
   else
   array.join(", ")  
